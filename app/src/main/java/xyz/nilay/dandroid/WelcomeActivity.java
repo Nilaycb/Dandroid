96a47f64fe48fa735a0b0bc3afc994efe9d6ed88
Nilaycb <contact@nilay.xyz>
2023-06-05 13:48:19 +0600

    init

--- app/src/main/java/xyz/nilay/dandroid/WelcomeActivity.java
+++ app/src/main/java/xyz/nilay/dandroid/WelcomeActivity.java
@@ -3,11 +3,14 @@ package xyz.nilay.dandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     TextView showName;
+    Button goBackButton;
     String username;
 
     @Override
@@ -16,8 +19,16 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         showName = findViewById(R.id.showName);
+        goBackButton = findViewById(R.id.goBackButton);
 
         username = getIntent().getStringExtra("username").toString();
         showName.setText("Welcome "+username);
+
+        goBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
